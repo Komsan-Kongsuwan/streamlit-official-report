5f3be21e74ef52f9db1b17325d67e7bd828b68ab
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-06 21:45:53 +0700

    Update 3_BPB.py

--- pages/3_BPB.py
+++ pages/3_BPB.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title("📈 Chart Visualization Revenue")
+st.title("📈 Chart Visualization")
 
 # --- Load session data ---
 if "official_data" not in st.session_state:
