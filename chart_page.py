ce3bfbf8e5ec486a6d735fac183364cc265cb0bd
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-11 08:19:07 +0700

    Update chart_page.py

--- chart_page.py
+++ chart_page.py
@@ -103,7 +103,7 @@ def render_chart_page(site_code):
             "Rating": rating
         })
 
-    st.markdown(f"### 📊 Comparison {prior_month} vs {latest_month}")
+    st.markdown(f"### 📊 Comparison {prior_month.strftime("%b")} vs {latest_month.strftime("%b")}")
     row_chunks = [comparison_data[i:i+4] for i in range(0, len(comparison_data), 4)]
     for row in row_chunks:
         cols = st.columns(4)
