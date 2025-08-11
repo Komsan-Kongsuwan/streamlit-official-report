f8b435198407c8eedd585afdc1c1127f2959e8fd
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-11 08:07:38 +0700

    Update chart_page.py

--- chart_page.py
+++ chart_page.py
@@ -104,7 +104,7 @@ def render_chart_page(site_code):
         })
 
     current_month = df_selected['Period'].max().strftime("%b"),
-    before_month = latest_month - pd.DateOffset(months=1).strftime("%b")
+    before_month = current_month - pd.DateOffset(months=1).strftime("%b")
     st.markdown(f"### 📊 Comparison {before_month} vs {current_month}")
     row_chunks = [comparison_data[i:i+4] for i in range(0, len(comparison_data), 4)]
     for row in row_chunks:
