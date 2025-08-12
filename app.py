e26b0ba8f81a1719c1aaac089dc1975d2f72632f
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-12 15:32:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Monthly Report", layout="wide")
+st.set_page_config(page_title="Monthly Official Report", layout="wide")
 st.title("📊 Welcome to the Monthly Report App")
 
 st.markdown("Select a report from the sidebar.")
