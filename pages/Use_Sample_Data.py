269ae0d1792333c7b348dd17fdb7701fd9320a40
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-16 19:52:16 +0700

    Update Use_Sample_Data.py

--- pages/Use_Sample_Data.py
+++ pages/Use_Sample_Data.py
@@ -5,10 +5,6 @@ import pandas as pd
 sample_file = os.path.join(os.getcwd(), "official_raw_data.xlsx")
 if os.path.exists(sample_file):
     df_sample = pd.read_excel(sample_file)
-    df_sample['Period'] = pd.to_datetime(
-        df_sample['Year'].astype(str) + "-" + df_sample['Month'].astype(str).str.zfill(2),
-        format="%Y-%m"
-    )
     st.session_state["official_data"] = df_sample
     st.success("✅ Sample data loaded successfully. Go to Chart page to view it!")
 else:
