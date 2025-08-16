a689dc36951cea26f80caa7fe5b6510d398969ef
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-16 23:43:54 +0700

    Update chart_page.py

--- chart_page.py
+++ chart_page.py
@@ -161,9 +161,9 @@ def render_chart_page():
 
     # --- Comparison Summary Inline (7 boxes in one line) ---
     st.markdown(f"""
-        <h3 style='margin-top:0; margin-bottom:0.5rem; color:#333;'>
+        <h4 style='margin-top:0; margin-bottom:0.5rem; color:#333;'>
             Site : {site_code} - Visualize Revenue/Cost/Profit - {latest_month.strftime('%B %Y')}
-        </h3>
+        </h4>
     """, unsafe_allow_html=True)
 
     cols = st.columns(7)  # 🔹 exactly 7 boxes
