227a2ce159f4969437970abd74918b6969166e05
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-16 13:03:36 +0700

    Update chart_page.py

--- chart_page.py
+++ chart_page.py
@@ -112,7 +112,7 @@ def render_chart_page():
 
     st.markdown(f"""
         <h3 style='margin-top:0; margin-bottom:0.5rem; color:#333;'>
-            {st.session_state.selected_site}
+            Site : {st.session_state.selected_site} Comparison {prior_month.strftime('%B %Y')} vs {latest_month.strftime('%B %Y')}
         </h3>
     """, unsafe_allow_html=True)
 
