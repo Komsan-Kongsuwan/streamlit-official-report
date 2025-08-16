cbffe67252468a4d4b8a21e1c8b1410980ffe447
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-16 23:37:57 +0700

    Update chart_page.py

--- chart_page.py
+++ chart_page.py
@@ -172,9 +172,9 @@ def render_chart_page():
         <div style="border:1px solid #ccc; border-radius:6px; padding:6px;
                     background-color:#f9f9f9; box-shadow:1px 1px 3px rgba(0,0,0,0.1);
                     font-size:11px;">
-            <h5 style="font-size:10px; margin-bottom:4px; color:#333;">
+            <p style="font-size:10px; margin-bottom:4px; color:#333;">
                 {data['Item']} {data['Rating']}
-            </h5>
+            </p>
             <p style="margin:2px 0; font-size:12px;"><b>{data['Month2']}:</b> 
                 <span style="color:black;">{data['Previous']}</span></p>
             <p style="margin:2px 0; font-size:12px; font-weight:bold;"><b>{data['Month1']}:</b> 
