da42eee72c9d2f42812d85b692225502a3f0e91e
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-16 11:04:08 +0700

    Update chart_page.py

--- chart_page.py
+++ chart_page.py
@@ -8,7 +8,7 @@ def render_chart_page():
     st.markdown("""
         <style>
             .block-container {
-                padding-top: 4rem;
+                padding-top: 2rem;
             }
         </style>
     """, unsafe_allow_html=True)
